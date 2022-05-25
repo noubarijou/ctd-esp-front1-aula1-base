e3cb7ab51aa337d0b11be6675169e63edb8c8c5f
noubarijou <85699669+noubarijou@users.noreply.github.com>
2022-05-25 08:57:26 -0300

    adds prop-typing

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,5 +1,6 @@
 import {useContext, useState} from "react";
 import { contextoFormulario } from "../../context/contextoFormulario";
+import PropTypes from 'prop-types';
 
 
 const Input = ({ name, label, type = "text", isPokemon = false }) => {
@@ -8,9 +9,22 @@ const Input = ({ name, label, type = "text", isPokemon = false }) => {
   
   const [input, setInput] = useState(form[name] || '');
 
+  /**
+  * Esta função recebe o evento como parametro e atualiza o estado do input ao passo que é modificado
+  * @param {event} event - evento do input
+  * @returns {void}
+  */
+
   const onChange = (e) => {
     setInput(e.target.value);
   };
+
+  /**
+   *Esta função recebe o evento como parametro e previne o recarregamento da página
+   *ela também atualiza o estado do contexto de acordo com o input inserido assim que o foco é retirado do input
+    *@param {event} event - evento do input
+    *@returns {void} 
+   */
 
   const onBlur = (e) => {
     e.preventDefault();
@@ -35,5 +49,12 @@ const Input = ({ name, label, type = "text", isPokemon = false }) => {
     </div>
   );
 };
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  isPokemon: PropTypes.bool,
+}
 
 export default Input;
--- src/context/contextoFormulario.jsx
+++ src/context/contextoFormulario.jsx
@@ -1,6 +1,10 @@
 // Aqui devemos criar nosso contexto e nosso provider.
 import { createContext, useReducer } from "react";
 
+/**
+ * Aqui criamos o contexto e o estado inicial
+ */
+
 export const contextoFormulario = createContext();
 const initialState = {
     treinador: {
@@ -16,6 +20,13 @@ const initialState = {
       idadePokemon: "",
     },
   };
+
+/**
+ * A função reducer é responsável por atualizar o estado do contexto.
+ * @param {*} state 
+ * @param {*} action 
+ * @returns {Object}
+ */
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -40,10 +51,18 @@ const reducer = (state, action) => {
     }
 };
 
+/**
+  * Declaramos o provider do contexto do formulário
+ */
+
 const ContextoFormularioProvider = ({ children }) => {
 
     const [form, dispatch] = useReducer(reducer, initialState);
-
+  /**
+   * A função recebe o action type e a informação do input e atualiza o estado do contexto pelo dispatch
+   * @param {*} type 
+   * @param {*} inputInfo 
+   */
     const displayOnBlur = (type, inputInfo) => {
         const {campo, valor} = inputInfo;
 
